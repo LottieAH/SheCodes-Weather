a4506fa52a1afd184b31dd74dd9f363ffc080446
LottieAH <93988986+LottieAH@users.noreply.github.com>
2022-01-05 17:36:49 +0000

    Initial Commit
    Issues so far: CORs policy block - hoping this is rectified by pushing to netlify!

---
+++ src/app.js
@@ -0,0 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
+
+let apiKey = "b76279664b73c37d7b7d0ba8dd209be6";
+let apiUrl = `api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
