b128793f6315ddf9f62f73ce8ecb0fc6c2dd2b38
LottieAH <93988986+LottieAH@users.noreply.github.com>
2022-01-14 12:08:39 +0000

    Forecast data now displaying, see still to do:
    Unit conversion
    Icons
    Background
    that random extra number!

--- src/app.js
+++ src/app.js
@@ -21,27 +21,42 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2">
           <div class="forecast-day">
-           ${day}
+           ${formatDay(forecastDay.dt)}
+          ${index}
           </br>
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" width="40"/>
+          <img src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png" 
+          alt="" 
+          width="40"/>
           </br>
-          <div class="high">18°</div>
-          <div class="low">12°</div>
+          <div class="high">${Math.round(forecastDay.temp.max)}°</div>
+          <div class="low">${Math.round(forecastDay.temp.min)}°</div>
           </div>
           </div>
       `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
